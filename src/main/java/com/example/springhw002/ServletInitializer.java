c100f9af143deae7c033bece7e002b38dd3f8d5c
Sopanha Yoeun <yoeunsopanha2001@gmail.com>
2021-05-25 19:55:07 +0700

    Initial commit

---
+++ src/main/java/com/example/springhw002/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springhw002;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringHw002Application.class);
+    }
+
+}
